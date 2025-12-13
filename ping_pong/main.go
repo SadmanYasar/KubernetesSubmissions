2bf3bad8024362dc6a1d6d95a019d04c837730b3
SadmanYasar <sadmanyasarsayem@gmail.com>
2025-12-13 19:58:09 +0800

    add pingpong service with go

---
+++ ping_pong/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var (
+	counter int
+	mu      sync.Mutex
+)
+
+func pingPongHandler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+	fmt.Fprintf(w, "pong %d", counter)
+	counter++
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/pingpong", pingPongHandler)
+	http.HandleFunc("/", rootHandler)
+
+	log.Printf("Server starting on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
